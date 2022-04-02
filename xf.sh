be17fdba436a9b7b1c5c6b672ec269c4ac140d1b
cicadacode <52892624+cicadacode@users.noreply.github.com>
2022-04-03 02:15:25 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "72e19b65-24e5-454d-8698-91854e52a135",
                         "alterId": 0
                     }
                 ],
